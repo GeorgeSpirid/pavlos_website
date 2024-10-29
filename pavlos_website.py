275821fdb3033a87b0caabf811bc08e5a280284e
itznot.george <spiridopoulosg@gmail.com>
2024-10-29 12:42:42 +0200

    fixed int(intput)

--- pavlos_website.py
+++ pavlos_website.py
@@ -1,4 +1,11 @@
 dna_sequence={'A':'T','T':'A','G':'C','C':'G'}
-sequence=input()
-for char in sequence:
-    print(dna_sequence[char],end='')
+rna_sequence={'A':'U','U':'A','G':'C','C':'G'}
+choice=int(input("press 1 for DNA or 2 for RNA: "))
+if(choice==1):
+    sequence=input()
+    for char in sequence:
+        print(dna_sequence[char],end='')
+elif(choice==2):
+    sequence=input()
+    for char in sequence:
+        print(rna_sequence[char],end='')
